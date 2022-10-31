932ddc6d454427955694378b35aba95d2e5eb8c7
aximsiva <sivashankaran.k@aximsoft.com>
2022-10-31 16:35:10 +0530

    date and time picker implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
+import moment from 'moment';
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { SolidCalendar } from './component';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class='parent'>
+      <SolidCalendar
+        currentDate={moment().toDate()}
+        enableTodayNavigator
+        enableCalendarViewType
+        enableTimeView
+      />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './index.css';
+import './main.scss';
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
